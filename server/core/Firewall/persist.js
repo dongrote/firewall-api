81ff6f9b4dae24729c0a0c83bdbbb659e59818a0
Don Grote <don.grote@gmail.com>
2020-04-16 14:43:03 -0400

    wait for file open

--- server/core/Firewall/persist.js
+++ server/core/Firewall/persist.js
@@ -2,19 +2,16 @@
 const fs = require('fs'),
   cp = require('child_process');
 
-exports = module.exports = persistPath => {
-  const outputStream = fs.createWriteStream(persistPath, {mode: 0o644}),
-    saveCommand = cp.spawn('iptables-save', [], {stdio: ['ignore', outputStream, 'ignore']});
-  return Promise.all([
-    new Promise((resolve, reject) => {
-      saveCommand
-        .on('error', reject)
-        .on('close', () => resolve());
-    }),
-    new Promise((resolve, reject) => {
-      outputStream
-        .on('error', reject)
-        .on('finish', () => resolve());
-    }),
-  ]);
-};
+exports = module.exports = persistPath => new Promise((resolve, reject) => {
+  const outputStream = fs.createWriteStream(persistPath, {mode: 0o644})
+    .on('error', reject)
+    .on('open', () => {
+      const cmd = cp.spawn('iptables-save', [], {stdio: ['ignore', outputStream, 'ignore']});
+      return Promise.all([
+        new Promise((res, rej) => cmd.on('error', rej).on('close', () => res())),
+        new Promise((res, rej) => outputStream.on('error', rej).on('finish', () => res())),
+      ])
+      .then(() => resolve())
+      .catch(reject);
+    });
+});
