5a27b0679f2b5bd5a8b377d9d35ab849100c2c25
Don Grote <don.grote@gmail.com>
2020-04-18 21:45:22 -0400

    Don't let isBlocked throw an error
    Fixes #10

--- server/core/Firewall/inet4/isBlocked.js
+++ server/core/Firewall/inet4/isBlocked.js
@@ -1,11 +1,6 @@
 'use strict';
 const cp = require('child_process');
 
-exports = module.exports = hostname => new Promise((resolve, reject) => {
-  cp.execFile('iptables', ['-C', 'FORWARD', '-p', 'all', '-s', hostname, '-j', 'REJECT'], err => {
-    if (err) {
-      return err.code === 1 ? resolve(false) : reject(err);
-    }
-    resolve(true);
-  });
+exports = module.exports = hostname => new Promise(resolve => {
+  cp.execFile('iptables', ['-C', 'FORWARD', '-p', 'all', '-s', hostname, '-j', 'REJECT'], err => resolve(!Boolean(err)));
 });
