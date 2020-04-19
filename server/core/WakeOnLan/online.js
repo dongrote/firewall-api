0ee7f73b8d6f2b68a023dd176b9a51bff7e7d787
Don Grote <don.grote@gmail.com>
2020-04-19 11:44:29 -0400

    Update online.js
    I fix. ┬─┬ ノ( ^_^ノ)

--- server/core/WakeOnLan/online.js
+++ server/core/WakeOnLan/online.js
@@ -5,16 +5,16 @@ const cp = require('child_process'),
 exports = module.exports = () => new Promise((resolve, reject) => {
   let stdout = '';
   const child = cp.spawn('arp', ['-a', '-n'], {stdio: ['ignore', 'pipe', 'ignore']});
-  child.on('error', reject);
-  child.stdout
-    .on('data', chunk => {
-      stdout += chunk.toString();
-    })
-    .on('finish', () => {
+  child
+    .on('error', reject)
+    .on('exit', () => {
       const addresses = stdout.split('\n').map(line => {
         const m = line.match(regex);
         return m ? m[1] : null;
       });
       resolve(addresses.filter(a => !(a === null || a === '<incomplete>')));
     });
+  child.stdout.on('data', chunk => {
+    stdout += chunk.toString();
+  });
 });
