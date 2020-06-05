c2e0d36e04578be92a4362533857cf3486c9dd0b
Don Grote <don.grote@gmail.com>
2020-06-04 21:22:42 -0400

    Remove admin requirement for wakeonlan

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,6 +11,6 @@ const requireAdminRole = require('../middleware/requireRole')('admin'),
 router.get('/health', (req, res) => res.sendStatus(200));
 router.get('/api/inet4', requireAdminRole, mutateHostnameBlockStatus);
 router.get('/api/inet4/blocked', queryBlockedHostname);
-router.get('/api/ethernet/wol', requireAdminRole, wakeupHost);
+router.get('/api/ethernet/wol', wakeupHost);
 router.get('/api/ethernet/online', queryOnlineHosts);
 router.get('/api/ethernet/arp', queryHostHardwareAddress);
